b63e54f8639a5ad3ecf69334b1548c1ffae6b3a7
Dan Gardner <dan@minas.sink.me.uk>
2012-08-24 00:49:28 +0100

    Modified many function names and added lots of documentation

--- demo.py
+++ demo.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from pylcdsysinfo import Colours, TextColours, Alignment, LCDSysinfo
+from pylcdsysinfo import BackgroundColours, TextColours, TextAlignment, TextLines, LCDSysInfo
+from time import sleep
 
-d = LCDSysinfo()
-d.dim_lcd_during_idle(False)
-d.change_lcd_brightness(255)
-d.save_brightness_value_to_device(0, 255)
-d.clear_lines(63, Colours.BLACK)
-d.set_text_background_colour(Colours.BLACK)
-d.display_cpu(994, 32, TextColours.BRIGHT_WHITE, TextColours.WHITE)
-d.display_ram_gpu(1994, 32, TextColours.RED, TextColours.GREEN)
-d.display_network(1994, 1994, TextColours.RED, TextColours.GREEN, 0, 1) 
-d.display_fans(1994, 1994, TextColours.RED, TextColours.GREEN)
+d = LCDSysInfo()
+d.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
+d.dim_when_idle(False)
+d.set_brightness(255)
+d.save_brightness(127, 255)
+d.set_text_background_colour(BackgroundColours.BLACK)
+d.display_cpu_info(8010, 32, TextColours.RED, TextColours.WHITE)
+d.display_ram_gpu_info(1994, 32, TextColours.RED, TextColours.GREEN)
+d.display_network_info(1, 2, TextColours.RED, TextColours.GREEN, 0, 1) 
+d.display_fan_info(1994, 1994, TextColours.RED, TextColours.GREEN)
 for pos in range(0, 48):
-    d.display_icon_on_grid(pos, 1 + pos % 32)
-d.clear_lines(63, Colours.WHITE)
-d.set_text_background_colour(Colours.ORANGE)
+    d.display_icon(pos, 1 + pos % 32)
+d.clear_lines(63, BackgroundColours.WHITE)
+d.set_text_background_colour(BackgroundColours.BLUE)
+sleep(1)
 for line in range(1, 7):
-    d.display_text_on_line(line, "Hello|world", False, Alignment.LEFT, TextColours.GREEN)
+    d.display_text_on_line(line, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", False, TextAlignment.LEFT, TextColours.WHITE)
+sleep(5)
+d.set_brightness(128)
--- pylcdsysinfo.py
+++ pylcdsysinfo.py
@@ -6,7 +6,7 @@
 
 import usb, time
 
-font_length_table = [
+_font_length_table = [
     0x11, 0x06, 0x08, 0x15, 0x0E, 0x19, 0x15, 0x03, 0x08, 0x08, 0x0F, 0x0D,
     0x05, 0x08, 0x06, 0x0B, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11,
     0x11, 0x11, 0x06, 0x06, 0x13, 0x10, 0x13, 0x0C, 0x1A, 0x14, 0x10, 0x12,
@@ -18,6 +18,7 @@ font_length_table = [
 ]
 
 class TextColours(object):
+    """Colour palette for text colours"""
     GREEN       = 1
     YELLOW      = 2
     RED         = 3
@@ -40,8 +41,8 @@ class TextColours(object):
     NAVY        = 30
     LIGHT_GREEN = 31
 
-""" 16-bit (highcolor) colours """
-class Colours(object):
+class BackgroundColours(object):
+    """Palette of 16-bit (highcolor) background colours"""
     BLACK       = 0x0000
     BLUE        = 0x001f
     GREEN       = 0x07e0
@@ -56,19 +57,45 @@ class Colours(object):
     YELLOW      = 0xffe0
     WHITE       = 0xffff
 
-class Alignment(object):
+class TextAlignment(object):
+    """Text alignment specifiers"""
     CENTRE      = 0
     LEFT        = 1
     RIGHT       = 2
 
-class LCDSysinfo(object):
+class TextLines(object):
+    """Text line specifiers"""
+    LINE_1      = 1 << 0
+    LINE_2      = 1 << 1
+    LINE_3      = 1 << 2
+    LINE_4      = 1 << 3
+    LINE_5      = 1 << 4
+    LINE_6      = 1 << 5
+    ALL         = LINE_1 + LINE_2 + LINE_3 + LINE_4 + LINE_5 + LINE_6
+
+class LCDSysInfo(object):
+
+    """A Python driver for the Coldtears LCD Sys Info
+    (http://coldtearselectronics.wikispaces.com/)
+    """
+
     def __init__(self, index=0):
+        """Opens a handle to an LCD Sys Info device.
+
+        Args:
+            index (int): The index of the device in the list of connected LCD
+                Sys Info devices, with zero (the default) being the first device.
+        Raises:
+            IOError: An error ocurred while opening the LCD Sys Info device.
+            RuntimeError: PyUSB 0.4 or later is required.
+        """
+
         self.usb_timeout_ms = 2000
         self.clear_lines_wait_ms = 1000
 
         dev = self._find_device(0x16c0, 0x05dc, index)
         if not dev:
-            raise IOError("LCDSysinfo device not found")
+            raise IOError("LCD Sys Info device not found")
         self.devh = dev.open()
         if not self.devh:
             raise IOError("Failed to open device")
@@ -84,6 +111,7 @@ class LCDSysinfo(object):
                 raise IOError("Failed to claim interface")
 
     def __del__(self):
+        """Closes the handle to the LCD Sys Info device."""
         if self.devh:
             try:
                 self.devh.releaseInterface()
@@ -92,6 +120,7 @@ class LCDSysinfo(object):
                 pass
 
     def _find_device(self, idVendor, idProduct, index):
+        """Locate the index'th device with specified vendor and product id."""
         for bus in usb.busses():
             for dev in bus.devices:
                 if dev.idVendor == idVendor and dev.idProduct == idProduct:
@@ -100,74 +129,169 @@ class LCDSysinfo(object):
                         return dev
         return None
 
-    def change_lcd_brightness(self, value):
-        value = min(value, 255)
+    def set_brightness(self, value):
+        """Set the brightness of the LCD backlight without saving the value to the device.
+
+        Args:
+            value (int): Number representing the LCD brightness, in the range 0 to 255.
+        """
+        value = max(0, min(value, 255))
         self.devh.controlMsg(0x40, 13, "", min(value, 255), min(value, 255), self.usb_timeout_ms)
 
-    def save_brightness_value_to_device(self, off_value, on_value):
+    def save_brightness(self, off_value, on_value):
+        """Set the brightness of the LCD backlight when idle and active and
+        save the values to the device.
+
+        Args:
+            off_value (int): Number representing the LCD backlight brightness
+                when the LCD is idle.
+            on_value (int): Number representing the LCD backlight brightness
+                when the LCD is active.
+        """
         self.devh.controlMsg(0x40, 14, "", off_value + on_value * 256, 0, self.usb_timeout_ms)
 
-    def display_icon_on_grid(self, position, icon_number):
+    def display_icon(self, position, icon_number):
+        """Display an icon at a specified position on the device.
+
+        Args:
+            position (int): Number representing the position in which to display 
+                the icon, in the range 0 to 47, where 0 is the top-left corner
+                of the display and 48 is the bottom-right corner.
+            icon_number (int): The index of the icon to be displayed. This may be
+                in the range 1 to 32 for the default icons or 257+ for downloaded
+                icons. An invalid icon number will display garbage to screen.
+        """
+        # TODO create enumeration class for icons
+        position = max(0, min(position, 47))
         self.devh.controlMsg(0x40, 27, "", position * 512 + icon_number, 25600, self.usb_timeout_ms)
 
     def set_text_background_colour(self, colour):
+        """Set the background colour for text display.
+
+        Args:
+            colour (int): The background colour from pylcdsysinfo.BackgroundColours.
+        """
         self.devh.controlMsg(0x40, 30, "", colour, 0, self.usb_timeout_ms)
 
     def _align_text(self, mm, alignment, screen_px, string_length_px):
+        """Align text suitably for the specified screen width"""
         spaces,pixels = divmod(screen_px - string_length_px, 17)
-        if alignment == Alignment.CENTRE:
+        if alignment == TextAlignment.CENTRE:
             mm = mm.center(len(mm) + spaces, " ").center(len(mm) + pixels, "{")
-        elif alignment == Alignment.LEFT:
+        elif alignment == TextAlignment.LEFT:
             mm = mm + " " * spaces + "{" * pixels
-        elif alignment == Alignment.RIGHT:
+        elif alignment == TextAlignment.RIGHT:
             mm = " " * spaces + "{" * pixels + mm
         return mm
 
-    def _text_conversion(self, mm, leavespaceforicon, alignment):
-        screen_px = 281 if leavespaceforicon else 319
-        mm = mm.strip().replace(" ", "_")
+    def _text_conversion(self, mm, pad_for_icon, alignment):
+        """Pad, truncate, align and otherwise munge the specified text."""
+        screen_px = 281 if pad_for_icon else 319
+        mm = mm.strip().replace(" ", "___")
         string_length_px = 0
         for k in range(0, len(mm)):
             ascii_value = ord(mm[k])
             char_length_px = 0
             if ascii_value >= 32 and ascii_value <= 125:
-                char_length_px = font_length_table[ascii_value - 32]
+                char_length_px = _font_length_table[ascii_value - 32]
             if string_length_px + char_length_px > screen_px:
                 mm = mm[0:k]
                 break
             string_length_px += char_length_px
         return self._align_text(mm, alignment, screen_px, string_length_px)
 
-    def display_text_on_line(self, line, textstring, leavespaceforicon, alignment, colour):
-        textstring = self._text_conversion(textstring, leavespaceforicon, alignment)
-        textlength = len(textstring)
-        if not leavespaceforicon:
-            textlength += 256
-        if colour > 32:
-            colour = 0
-        if line < 1 or line > 6:
-            line = 1
-        self.devh.controlMsg(0x40, 24, textstring, textlength, (line - 1) * 256 + colour, self.usb_timeout_ms)
+    def display_text_on_line(self, line, text_string, pad_for_icon, alignment, colour):
+        """Display text on a line of the device.
 
-    def dim_lcd_during_idle(self, value):
+        Args:
+            line (int): The line on which the text should be displayed, in the range 1 to 6.
+            text_string (str): The text to be displayed, which will be truncated as
+                required. Use "|" for wider spacing and "^" to display a "degrees" symbol.
+            pad_for_icon (bool): If true, padding will be added to the left of the text,
+                to accommodate an icon.
+            alignment (int): The text alignment from pylcdsysinfo.TextAlignment.
+            colour (int): The text colour from pylcdsysinfo.TextColours.
+        """
+        text_string = self._text_conversion(text_string, pad_for_icon, alignment)
+        text_length = len(text_string)
+        if not pad_for_icon:
+            text_length += 256
+        colour = min(colour, 32)
+        line = max(1, min(line, 6))
+        self.devh.controlMsg(0x40, 24, text_string, text_length, (line - 1) * 256 + colour,
+            self.usb_timeout_ms)
+
+    def dim_when_idle(self, value):
+        """Set whether to dim the LCD backlight after the device has been idle for 10 seconds.
+
+        Args:
+            value (bool): If true, the LCD backlight will dim when the device is idle,
+                otherwise the function will be disabled.
+        """
         if value:
             self.devh.controlMsg(0x40, 17, "", 1, 0, self.usb_timeout_ms)
         else:
             self.devh.controlMsg(0x40, 17, "", 0, 266, self.usb_timeout_ms)
 
     def clear_lines(self, lines, colour):
-        # bits 0 - 5 of lines represent each LCD line
+        """Clear lines of the display using a coloured background.
+
+        Args:
+            lines (int): A number representing the lines to be cleared, using the
+                values from pylcdsysinfo.TextLines OR'd together to form bits 0 to 5.
+            colour (int): The background colour from pylcdsysinfo.BackgroundColours.
+        """
+        lines = max(1, min(lines, 63))
         self.devh.controlMsg(0x40, 26, "", lines, colour, self.usb_timeout_ms)
         time.sleep(self.clear_lines_wait_ms / 1000)
 
-    def display_cpu(self, usage, temp, usage_colour=TextColours.GREEN, temp_colour=TextColours.GREEN):
-        self.devh.controlMsg(0x40, 21, chr(usage_colour) + chr(temp_colour), usage, temp, self.usb_timeout_ms)
-        
-    def display_ram_gpu(self, ram, gpu, ram_colour=TextColours.GREEN, gpu_colour=TextColours.GREEN):
-        self.devh.controlMsg(0x40, 22, chr(ram_colour) + chr(gpu_colour), ram, gpu, self.usb_timeout_ms)
+    def display_cpu_info(self, cpu_util, cpu_temp, util_colour=TextColours.GREEN, temp_colour=TextColours.GREEN):
+        """Display CPU utilisation and temperature information.
 
-    def display_network(self, recv, sent, recv_colour=TextColours.GREEN, sent_colour=TextColours.GREEN, recv_mb=0, sent_mb=0):
+        Args:
+            cpu_util (int): Percentage CPU utilisation, to a maximum of 4 digits,
+                e.g. 994 will display 99.4%.
+            cpu_temp (int): CPU temperature in degrees Celsius, to a maximum of 2 digits,
+                e.g. 32 will display 32°C.
+            util_colour (int): The colour of the CPU utilitisation, from
+                pylcdsysinfo.BackgroundColours (defaults to GREEN).
+            temp_colour (int): The colour of the CPU temperature, from
+                pylcdsysinfo.BackgroundColours (defaults to GREEN).
+        """
+        self.devh.controlMsg(0x40, 21, chr(util_colour) + chr(temp_colour), cpu_util, cpu_temp, self.usb_timeout_ms)
+
+    def display_ram_gpu_info(self, ram, gpu_temp, ram_colour=TextColours.GREEN, temp_colour=TextColours.GREEN):
+        """Display available RAM and GPU temperature information.
+
+        Args:
+            ram (int): Available RAM in megabytes, to a maximum of 4 digits,
+                e.g. 1994 will display 1994Mb.
+            gpu_temp (int): GPU temperature in degrees Celsius, to a maximum of 2 digits,
+                e.g. 32 will display 32°C.
+            util_colour (int): The colour of the available RAM, from
+                pylcdsysinfo.BackgroundColours (defaults to GREEN).
+            temp_colour (int): The colour of the GPU temperature, from
+                pylcdsysinfo.BackgroundColours (defaults to GREEN).
+        """
+        self.devh.controlMsg(0x40, 22, chr(ram_colour) + chr(temp_colour), ram, gpu_temp, self.usb_timeout_ms)
+
+    def display_network_info(self, recv, sent, recv_colour=TextColours.GREEN, sent_colour=TextColours.GREEN, recv_mb=0, sent_mb=0):
+        """Display network utilisation information.
+
+        Args:
+            recv (int): Current network receive rate, to a maximum of 4 digits,
+                e.g. 1994 will display 1994Mb.
+            sent (int): Current network transmit rate, to a maximum of 4 digits,
+                e.g. 1994 will display 1994Mb.
+            recv_colour (int): The colour of the network receive rate, from
+                pylcdsysinfo.BackgroundColours (defaults to GREEN).
+            sent_colour (int): The colour of the network transmit rate, from
+                pylcdsysinfo.BackgroundColours (defaults to GREEN).
+        """
+        # TODO sort out mb/kb (may need to power cycle)
+        recv_mb = 1
+        sent_mb = 1
         self.devh.controlMsg(0x40, 20, chr(recv_mb) + chr(sent_mb) + chr(recv_colour) + chr(sent_colour), recv, sent, self.usb_timeout_ms)
 
-    def display_fans(self, cpufan, chafan, cpufan_colour=TextColours.GREEN, chafan_colour=TextColours.GREEN):
+    def display_fan_info(self, cpufan, chafan, cpufan_colour=TextColours.GREEN, chafan_colour=TextColours.GREEN):
         self.devh.controlMsg(0x40, 23, chr(cpufan_colour) + chr(chafan_colour), cpufan, chafan, self.usb_timeout_ms)

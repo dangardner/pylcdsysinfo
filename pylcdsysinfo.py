18090794e63ca4ef7e0447c2fc7ac1b1dbbbf4ad
Dan Gardner <dan@minas.sink.me.uk>
2012-09-04 00:27:45 +0100

    Added GPLv3 boilerplate

--- pylcdsysinfo.py
+++ pylcdsysinfo.py
@@ -1,8 +1,22 @@
 # -*- coding: UTF-8 -*-
 #
-# Interface with LCD Sys info
+# Interface with LCD Sys info USB device
+# 
+# Copyright (C) 2012  Dan Gardner
 #
-# Some code shamelessly borrowed from https://code.google.com/p/pywws/
+# USB initialisation code has been adapted from pywws by Jim Easterbrook
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# See <http://www.gnu.org/licenses/lgpl-3.0.txt>
 
 import usb, time
 

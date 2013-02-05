533de00dd6d13adf29b8f4fdad5c6ababf7b5aac
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2013-02-05 11:03:54 -0500

    Spiff up the demo script a bit.
    - Use nicer formatting for the code
    - Demo a reverse-order incremental clearing of the screen

--- demo.py
+++ demo.py
@@ -9,22 +9,36 @@ d.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
 d.dim_when_idle(False)
 d.set_brightness(255)
 d.save_brightness(127, 255)
+
+# System Info
 d.set_text_background_colour(BackgroundColours.BLACK)
 d.display_cpu_info(8010, 32, TextColours.RED, TextColours.WHITE)
 d.display_ram_gpu_info(1994, 32, TextColours.RED, TextColours.GREEN)
 d.display_network_info(1, 2, TextColours.RED, TextColours.GREEN, False, True)
 d.display_fan_info(1994, 1994, TextColours.RED, TextColours.GREEN)
+
+# All icons
 for pos in range(0, 48):
     d.display_icon(pos, 1 + pos)
+sleep(1)
+
+# Arbitrary text drawing
 d.clear_lines(TextLines.ALL, BackgroundColours.WHITE)
 d.set_text_background_colour(BackgroundColours.BLUE)
 sleep(1)
 for line in range(1, 7):
     d.display_text_on_line(line, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", False, TextAlignment.LEFT, TextColours.WHITE)
 sleep(1)
-d.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
+
+# Clearing individual lines
+for i in range(5,-1,-1):
+    d.clear_lines(1 << i, BackgroundColours.BLACK)
+
+# Image Drawing
 d.set_text_background_colour(BackgroundColours.BLACK)
 d.display_icon(0, 218)
+
+# Multi-color two-column drawing
 for line in range(1, 7):
     ipos = (line - 1) * 8
     icon = (line * 2) + 10
@@ -37,8 +51,12 @@ for line in range(1, 7):
         d.display_text_on_line(line, 'Row', True, TextAlignment.RIGHT, TextColours.GREEN, 3)
     d.display_icon(ipos + 4, icon + 1)
 sleep(1)
+
+# Refresh the background
 d.set_text_background_colour(BackgroundColours.BLACK)
 d.display_icon(0, 218)
+
+# Single-color two-column drawing at almost twice the speed
 for line in range(1, 7):
     ipos = (line - 1) * 8
     icon = (line * 2) + 10

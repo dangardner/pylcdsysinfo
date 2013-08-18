8cedecb01919d03d601319eea06c67c30fc87fae
Donovan Schonknecht <don@tpyo.net>
2013-08-18 15:51:29 +0200

    Use the LCDSysInfo variable name in MPD example for consistency

--- examples/mpd.py
+++ examples/mpd.py
@@ -40,34 +40,34 @@ def mpd_song_time(time):
 
 
 def lcd_connect():
-	lcd = LCDSysInfo()
-	lcd.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
-	lcd.dim_when_idle(True)
-	lcd.set_brightness(255)
-	lcd.save_brightness(127, 255)
-	lcd.set_text_background_colour(BackgroundColours.BLACK)
-	return lcd
+	d = LCDSysInfo()
+	d.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
+	d.dim_when_idle(True)
+	d.set_brightness(255)
+	d.save_brightness(127, 255)
+	d.set_text_background_colour(BackgroundColours.BLACK)
+	return d
 
 
-def lcd_clear_lines(lcd):
-	lcd.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
+def lcd_clear_lines(d):
+	d.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
 
 
-def lcd_update_song_time(lcd, elapsed):
-	lcd.display_text_on_line(5, elapsed, False, TextAlignment.LEFT, TextColours.WHITE)
+def lcd_update_song_time(d, elapsed):
+	d.display_text_on_line(5, elapsed, False, TextAlignment.LEFT, TextColours.WHITE)
 
 
-def lcd_update_current_song(lcd, song):
-	lcd.display_text_on_line(1, '', False, TextAlignment.LEFT, TextColours.WHITE)
-	lcd.display_text_on_line(2, song['title'], False, TextAlignment.LEFT, TextColours.WHITE)
-	lcd.display_text_on_line(3, song['artist'], False, TextAlignment.LEFT, TextColours.WHITE)
-	lcd.display_text_on_line(4, song['album'], False, TextAlignment.LEFT, TextColours.WHITE)
-	lcd.display_text_on_line(5, '0:01', False, TextAlignment.LEFT, TextColours.WHITE)
-	lcd.display_text_on_line(6, '', False, TextAlignment.LEFT, TextColours.WHITE)
+def lcd_update_current_song(d, song):
+	d.display_text_on_line(1, '', False, TextAlignment.LEFT, TextColours.WHITE)
+	d.display_text_on_line(2, song['title'], False, TextAlignment.LEFT, TextColours.WHITE)
+	d.display_text_on_line(3, song['artist'], False, TextAlignment.LEFT, TextColours.WHITE)
+	d.display_text_on_line(4, song['album'], False, TextAlignment.LEFT, TextColours.WHITE)
+	d.display_text_on_line(5, '0:01', False, TextAlignment.LEFT, TextColours.WHITE)
+	d.display_text_on_line(6, '', False, TextAlignment.LEFT, TextColours.WHITE)
 
 
 def main():
-	lcd = lcd_connect()
+	d = lcd_connect()
 
 	mpd = mpd_connect(HOST, PORT, PASSWORD)
 	if mpd is not False:
@@ -78,14 +78,14 @@ def main():
 
 			if 'title' in song:
 				if last != song:
-					lcd_update_current_song(lcd, song)
+					lcd_update_current_song(d, song)
 
 				if 'elapsed' in status:
 					elapsed = mpd_song_time(status['elapsed'])
-					lcd_update_song_time(lcd, elapsed)
+					lcd_update_song_time(d, elapsed)
 
 			else:
-				lcd_clear_lines(lcd)
+				lcd_clear_lines(d)
 			last = song
 
 			sleep(1)
